b92230c9af4125342a42f8b29ccf7c4deb19f150
Paul Kondratov <paulislava@mail.ru>
2026-07-12 04:20:35 +0500

    feat(web): allow next/image to load from new cdn.paulislava.space

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'cdn.paulislava.space',
+      },
+      {
+        protocol: 'https',
         hostname: 'cdn.beznomera.net',
       },
     ],
